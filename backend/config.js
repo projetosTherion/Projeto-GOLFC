c88e1495a4691919e82207c300fdcbb1b0032bd8
projetosTherion <159710027+projetosTherion@users.noreply.github.com>
2024-09-02 16:00:52 -0300

    Update config.js

--- backend/config.js
+++ backend/config.js
@@ -1,5 +1,6 @@
 export const PORT = 5555;
 
 export const mongodbUrl = 
-'mongodb+srv://test:MUQRIjZKTtqYGlhs@cluster0.c4ruu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+'mongodb+srv://contagolfc:ayEe1Lhhbtwmx0ih@clustergol.oq454.mongodb.net/?=true&w=majority&appName=ClusterGol'
+
 
