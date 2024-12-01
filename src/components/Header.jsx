4610b32ddfce94da2cb1626da993429da721aa56
armassaro <arthurmassaro@alunos.utfpr.edu.br>
2024-12-01 20:41:02 -0300

    correção da pagina de certificação

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ const Header = () => {
     <header className="shadow-2xl  relative  bg-opacity-90  bg-white text-black pr-4">
       <div className="flex justify-between items-center">
         {/* Logo */}
-        <div className='bg-Azul-Gol md:size-[4%] p-[0.5%] flex items-center justify-center'>
+        <div className='bg-Azul-Gol size-[4%] p-[0.5%] flex items-center justify-center'>
           <img src={Logo} alt="Logo Gol" />
         </div>
         {/* Menu de navegação */}
--- src/paginas/Certificacao.jsx
+++ src/paginas/Certificacao.jsx
@@ -1,9 +1,10 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import HeaderHomeLogado from "../components/HeaderHomeLogado";
 import Footer from "../components/Footer";
 import { Link } from 'react-router-dom';
 import FundoCertificacao from '../imagens/FundoCertificacao.png';
 import Certificado from '../imagens/Certificado.png';
+import { IoIosArrowBack } from "react-icons/io";
 
 const Login = () => {
   
@@ -11,26 +12,96 @@ const Login = () => {
     document.title = "Certificacao - GolFC";
   }, []);
 
-  return (
-    <div style={styles.container}>
-      <div 
-        className="md:absolute top-0 left-0 w-full max-w-[100vw] h-auto z-50 bg-Azul-Gol">
-        <HeaderHomeLogado />
-      </div>
+  const navRef = useRef(null);
+  const [navHeight, setNavHeight] = useState(0);
+  const styles = {
+    container: {
+      backgroundImage: `url(${FundoCertificacao})`,
+      backgroundSize: 'cover',
+      backgroundPosition: 'center',
+      backgroundRepeat: 'no-repeat',
+      minHeight: '150vh',
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+      padding: '20px',
+      paddingBottom: '100px',
+      position: 'relative',
+    },
+    
+    // Estilo do Header
+    headerContainer: {
+      width: '100%',
+      height: '40px',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+      position: 'absolute',
+      top: 0,
+      zIndex: 1000,
+    },
+  
+    backButtonContainer: {
+      position: 'absolute',
+      top: '13%',
+    },
+    
+    content: {
+      paddingTop: navHeight,
+      paddingBottom: "10%",
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'center',
+      alignItems: "center",
+      position: "relative"
+    },
+  
+    certificadoContainer: {
+      position: 'relative',
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    
+    certificado: {
+      width: '800px',
+    },
+  };
 
-      <div style={styles.backButtonContainer}>
-        <button className="bg-white text-[#0A1835] absolute bottom-0 left-[5%] py-1 px-2 font-Carbona rounded-2xl flex items-center">
-          Voltar
-        </button>
+  useEffect(() => {
+    if(navRef.current) {
+      setNavHeight(navRef.current.clientHeight);
+    }
+  }, [navRef]);
+
+  return (
+    <div
+    className='bg-Azul-Gol relative'>
+      <img 
+      src={FundoCertificacao}
+      className='bg-cover absolute top-0 left-0'/>
+      <div 
+        ref={navRef}
+        className="md:absolute top-0 left-0 w-full max-w-[100vw] h-auto z-50 bg-Azul-Gol filter backdrop-blur-xl bg-opacity-50">
+        <HeaderHomeLogado />
       </div>
       
       <div style={styles.content}>
-        <h1 className="text-white text-3xl font-tuskerGrotesk mb-4">
-          PARABÉNS,
-        </h1>
-        <h1 className="text-white text-lg font-tuskerGrotesk">
-          VOCÊ AGORA DETÉM UM COLECIONÁVEL ÚNICO E EXCLUSIVO. ESSA LEMBRANÇA ESPECIAL É SUA.
-        </h1>
+        <div className="flex flex-col text-start mt-[10%] relative">
+        <button className="bg-Azul-Gol text-white absolute top-0 -translate-y-[300%] left-0 py-1 px-[8px] font-Carbona rounded-[8px] font-[900] tracking-widest flex items-center
+          border-[1px] border-[#44D62D] border-solid text-[85%]">
+            <IoIosArrowBack
+            className="mr-1 hover:animate-pulse"
+            size={20}/>
+            Voltar
+          </button>
+          <h1 className="text-white text-3xl font-tuskerGrotesk z-10">
+            PARABÉNS,
+          </h1>
+          <h1 className="text-white text-lg font-tuskerGrotesk z-10 mb-[2%]">
+            VOCÊ AGORA DETÉM UM COLECIONÁVEL ÚNICO E EXCLUSIVO. ESSA LEMBRANÇA ESPECIAL É SUA.
+          </h1>
+        </div>
 
         {/* Container para imagem com texto sobreposto */}
         <div style={styles.certificadoContainer}>
@@ -42,74 +113,5 @@ const Login = () => {
     </div>
   );
 }
-
-const styles = {
-  container: {
-    backgroundImage: `url(${FundoCertificacao})`,
-    backgroundSize: 'cover',
-    backgroundPosition: 'center',
-    backgroundRepeat: 'no-repeat',
-    minHeight: '150vh',
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    padding: '20px',
-    paddingBottom: '100px',
-    position: 'relative',
-  },
-  
-  // Estilo do Header
-  headerContainer: {
-    width: '100%',
-    height: '40px',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-    position: 'absolute',
-    top: 0,
-    zIndex: 1000,
-  },
-
-  backButtonContainer: {
-    position: 'absolute',
-    top: '13%',
-    left: '25%',
-  },
-  
-  content: {
-    textAlign: 'center',
-    flex: 1,
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    justifyContent: 'center',
-    marginTop: '230px', // Aumenta o espaço entre o header e a imagem
-    marginBottom: '80px', // Aumenta o espaço entre a imagem e o footer
-  },
-
-  certificadoContainer: {
-    position: 'relative',
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-
-  overlayText: {
-    position: 'absolute',
-    color: 'black',
-    fontSize: '24px',
-    fontWeight: 'bold',
-  },
-  
-  certificado: {
-    width: '800px',
-    marginTop: '5px',
-  },
-  
-  footer: {
-    width: '',
-    marginTop: '', // Aumenta o espaço entre a imagem e o footer
-  },
-};
 
 export default Login;
