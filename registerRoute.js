c1ed9b0b53f247b77ee0a581897d42caf599e98f
Eduardo Yuji <65960933+eduardo-yuji@users.noreply.github.com>
2024-12-02 22:29:38 -0300

    Add files via upload

---
+++ registerRoute.js
@@ -0,0 +1,95 @@
+import express from 'express';
+import { User } from '../models/userModel.js';
+//import jwt from 'jsonwebtoken';
+import axios from 'axios';
+import jwtUtils from '../middleware/auth.js';
+import { enviarEmailValidacao } from '../routes/verificaEmailRoute.js';
+//var SibApiV3Sdk = require('sib-api-v3-sdk');
+//var defaultClient = SibApiV3Sdk.ApiClient.instance;
+
+const router = express.Router();
+
+// const sendVerificationEmail = async (userEmail, token) => {
+//  const apiKey = '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'; // Substitua pela sua chave de API do Brevo
+
+//   const emailData = {
+//    sender: { email: '7e156a001@smtp-brevo.com', name: 'GOLFC' },
+//     to: [{ email: userEmail }],
+//     subject: 'Confirme seu email',
+//     htmlContent: `
+//       <p>Olá,</p>
+//       <p>Clique no link abaixo para verificar seu email:</p>
+//       <a href="http://yourapp.com/verify-email?token=${token}">Verificar Email</a>
+//     `
+//   };
+
+//   try {
+//     await axios.post('https://api.brevo.com/v3/smtp/email', emailData, {
+//       headers: { 'api-key': apiKey, 'Content-Type': 'application/json' }
+//     });
+//     console.log('Email de verificação enviado.');
+//   } catch (error) {
+//    console.error('Erro ao enviar email:', error.response ? error.response.data : error.message);
+//   }
+// };
+
+// Rota para criar um novo usuário
+router.post("/", async (req, res, next) => {
+  try {
+    const {
+      nome, email, senha, data_nascimento, documentoID, celular, cep,
+      pais, estado, cidade, bairro, rua, numero, complemento, time_do_usuario
+    } = req.body;
+
+    // Verificação dos campos obrigatórios
+    if (
+      !nome || !email || !senha || !data_nascimento || !documentoID ||
+      !celular || !cep || !pais || !estado || !cidade || !bairro || !rua || !numero || !time_do_usuario
+    ) {
+      console.log('Campos obrigatórios ausentes:', req.body);
+      return res.status(400).send({
+        message: "Send all required fields",
+      });
+    }
+
+<<<<<<< HEAD
+=======
+    console.log('Dados recebidos para criar o usuário:', req.body);
+
+>>>>>>> f92ab327240b554219fed3d66815fe9f954adce7
+    // Criação do novo usuário com os dados recebidos
+    const newUser = {
+      nome, email, senha, data_nascimento, documentoID, celular, cep,
+      pais, estado, cidade, bairro, rua, numero, complemento, time_do_usuario,
+      isVerified: false // O email ainda não foi verificado
+    };
+
+    const user = await User.create(newUser);
+
+<<<<<<< HEAD
+    // const infoCliente = {
+    //   userId: user._id,
+    //   email: user.email,
+    // };
+    // Gera token JWT para verificação do email
+    // const token = jwtUtils.generateTokenVerificyEmail(infoCliente);
+    
+    //const token = jwt.sign({ userId: user._id, email: user.email }, 'secret_key', { expiresIn: '1h' });
+
+    // Envia email de verificação
+    
+    await enviarEmailValidacao(user.email);
+=======
+    console.log('Usuário criado com sucesso:', user);
+
+>>>>>>> f92ab327240b554219fed3d66815fe9f954adce7
+    // Retornando o usuário criado
+    return res.status(201).send({ message: "Usuário criado. Verifique seu email." });
+
+  } catch (err) {
+    console.log('Erro ao criar usuário:', err.message);
+    next(err); // Encaminha o erro para o middleware de tratamento de erros
+  }
+});
+
+export default router;
